7075c95b1f788c07b7317088c4a5978ad87f7b0e
Carson Sprock <csprock@gmail.com>
2022-07-24 20:50:06 +0000

    version 0.1.0

--- inst/doc/basic_usage.R
+++ inst/doc/basic_usage.R
@@ -25,21 +25,17 @@ result <- rwclust(example1, iter=6, k=3)
 
 ## ----plot_edge_weights, fig.align='center', fig.width=7, fig.height=5---------
 G_sharpened <- igraph::graph_from_edgelist(as.matrix(example1[, c(1,2)]), directed=FALSE)
-E(G_sharpened)$weights <- round(result$weights,1)
+E(G_sharpened)$weights <- round(weights(result),0)
 plot(G_sharpened, edge.label=E(G_sharpened)$weights, vertex.color=labels, layout=layout_with_fr)
 
 ## ----plot_histogram, fig.align='center', fig.width=7, fig.height=5------------
-hist(result$weights, breaks=20, 
-     main="Distribution of Final Edge Weights",
-     xlab="Edge Weight")
-abline(v=25, col="red")
-
+plot(result, cutoff=25, breaks=20)
 
 ## ----compute_components, fig.align='center', fig.width=7, fig.height=5--------
 # delete edges with weights below the threshold
-edges_to_keep <- which(result$weights > 25)
+edges_to_keep <- which(weights(result) > 25)
 example1_c <- example1[edges_to_keep, ]
-example1_c$weight <- result$weights[edges_to_keep]
+example1_c$weight <- weights(result)[edges_to_keep]
 
 G_c <- igraph::graph_from_edgelist(as.matrix(example1_c[, c(1,2)]), directed=FALSE)
 
